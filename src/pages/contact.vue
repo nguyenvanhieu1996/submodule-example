4ae30f81100752696aa2f4b85fbc4d4726871e9a
hieu.nv <>
2023-09-18 09:52:34 +0700

    first commit

---
+++ src/pages/contact.vue
@@ -0,0 +1,14 @@
+<template>
+    <Contact />
+</template>
+
+
+<script lang="ts">
+import { Component } from 'nuxt-property-decorator'
+
+@Component({
+  components: {
+    Contact: () => import('src/modules/templates/contact.vue')
+  }
+})
+</script>
